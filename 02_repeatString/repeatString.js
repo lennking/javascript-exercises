3938f24da59e9ef64ad04d7acae9080358db9401
Viet Le <popart683@gmail.com>
2024-12-10 15:11:23 -0800

    Implement string repetition function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(input, j) {
+    let output = "";
+    if (j < 0) {
+        return 'ERROR';
+    }
+    for (let i=0; i < j; i++) {
+        output += input;
+    }
+    return output;
 };
 
 // Do not edit below this line
