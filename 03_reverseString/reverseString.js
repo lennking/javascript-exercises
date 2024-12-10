843ed7f5038ad0245c10dd7c83947bb5175b45d2
lennking <vikkerz456@gmail.com>
2024-12-10 15:34:17 -0800

    Implement reversal function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    const reversed = [];
+    //length = input.length();
+    for (let i=input.length; i>0; i--) {
+        reversed.push(input[i-1]);
+    }
+    return reversed.join("");
 };
 
 // Do not edit below this line
