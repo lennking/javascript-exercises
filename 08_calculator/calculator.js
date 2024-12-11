f063f1819c7933e4afc86ad1bd580fccb5e4c50e
lennking <vikkerz456@gmail.com>
2024-12-10 18:44:00 -0800

    Implement algebra functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  solution = 0;
+	for (let i=0; i < array.length; i++) {
+    solution += array[i];
+  }
+  return solution;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  solution = array[0];
+	for (let i=1; i < array.length; i++) {
+    solution *= array[i];
+  }
+  return solution;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(input) {
+	solution = 1;
+  for (let i = input; i > 0; i--) {
+    solution = solution * i;
+  }
+  return solution;
 };
 
 // Do not edit below this line
