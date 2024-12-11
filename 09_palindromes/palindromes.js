1849ab7cf191dc7de239df10ccc1a99eb08f61f9
lennking <vikkerz456@gmail.com>
2024-12-11 00:17:19 -0800

    Implement palindrome checker function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    input = input.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, "");
+    input = input.replace(/\s+/g, '');
+    input = input.toLowerCase();
+    let j = 0;
+    for (let i=0; i<input.length; i++) {
+        if (input[i]===input[input.length-i-1]){
+            j++;
+            continue;
+        }
+        break;
+    }
+    if (j==input.length) {
+        return true;
+    }
+    return false;
+  
 };
 
 // Do not edit below this line
