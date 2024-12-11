2a8a564836c30945b9f4cbeb06bafd3b35e041c0
lennking <vikkerz456@gmail.com>
2024-12-10 17:22:24 -0800

    Implement summing function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(arg1, arg2) {
+    if (arg1<0 || arg2 <0 || !Number.isInteger(arg1) || !Number.isInteger(arg2)) {
+        return "ERROR";
+    }
+    if (arg2 > arg1) {
+        difference = arg2-arg1;
+        small = arg1;
+    } else {
+        difference = arg1-arg2;
+        small = arg2;
+    }
+    difference++;
+    sum = 0;
+    for (let i=0; i<difference; i++) {
+        sum += (small + i);
+    }
+    return sum;
 };
 
 // Do not edit below this line
