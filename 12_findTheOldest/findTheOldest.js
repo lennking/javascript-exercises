5aec471974852121c661cb869dcd1aa89ece8a7a
lennking <vikkerz456@gmail.com>
2024-12-11 02:17:58 -0800

    Create functions to find and compare ages

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+   return people.reduce((oldest, person) => {
+    let oldestAge = getAge(oldest.yearOfBirth,oldest.yearOfDeath);
+    let currentAge = getAge(person.yearOfBirth,person.yearOfDeath);
+    if (currentAge>oldestAge) {
+        return person;
+    }
+    return oldest;
+   });
 };
+
+const getAge = function(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death-birth;
+}
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
