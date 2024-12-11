e815d7518c19068770e72215525a55e3796f162d
lennking <vikkerz456@gmail.com>
2024-12-11 00:33:07 -0800

    Implement fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(max) {
+    if (max < 0) {
+        return "OOPS";
+    }
+    max = max.toString();
+    let i = 0;
+    let temp1 = 1;
+    let temp2 = 0;
+    let solution = 0;
+    while (i < max) {
+        solution = temp1 + temp2;
+        temp1=temp2;
+        temp2=solution;
+        i++;
+    }
+    return solution;
 };
 
 // Do not edit below this line
